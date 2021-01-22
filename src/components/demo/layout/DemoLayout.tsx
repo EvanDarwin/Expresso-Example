5a443864f2c8ae1dd12d8e0d983471b95e4e0b84
Evan Darwin <evan@relta.net>
2021-01-22 12:34:18 -0800

    Update expresso version, fixes

--- src/components/demo/layout/DemoLayout.tsx
+++ src/components/demo/layout/DemoLayout.tsx
@@ -2,10 +2,13 @@ import {Component, h} from "preact";
 
 export class DemoLayout extends Component {
     render() {
+        throw new Error("yikes");
         return <html lang="en">
         <head>
             <title>Expresso Example Application</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta name="description" content="Welcome to the demo of the expresso framework"/>
         </head>
         <body>{this.props.children}</body>
         </html>
