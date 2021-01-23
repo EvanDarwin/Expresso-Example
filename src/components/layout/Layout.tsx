bd0287f9f8e77e34b73913ff7b177e3711c83e40
Evan Darwin <evan@relta.net>
2021-01-23 15:47:14 -0800

    Simplify layout for easier refactoring, added static/

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,16 @@
 import {Component, h, VNode} from "preact";
 
-export class DemoLayout extends Component {
+interface Props {
+    title?: string;
+    titleSuffix?: string;
+}
+
+export class Layout extends Component<Props> {
     render(): VNode {
+        const {title, titleSuffix = "Expresso Framework"} = this.props;
         return <html lang="en">
         <head>
-            <title>Expresso Example Application</title>
+            <title>{title ? title + ` | ` : ''}{titleSuffix}</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="description" content="Welcome to the demo of the expresso framework"/>
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,19 @@
 import expresso, {connect, ExpressoRequest, ExpressoResponse, middleware} from "expresso";
 import {h} from "preact";
-import {DemoHomepage} from "./components/demo";
+import {Homepage} from "./components";
 
 // connect() loads the database connection
 connect().then(() => {
-    const app = expresso({
-        env: {
-            CUSTOM_TEST: {
-                default: "environment variable!",
-                required: true,
-            }
-        }
-    });
-
+    const app = expresso();
+    app.use(middleware.parsers.urlencoded())
     app.use(middleware.helmet())
-    app.use(middleware.parsers.raw())
-    app.use(middleware.parsers.json())
 
     // demo homepage
     app.get('/', async (req: ExpressoRequest, res: ExpressoResponse) => {
-        res.send(<DemoHomepage req={req}/>)
+        res.send(<Homepage req={req}/>)
     })
 
-    app.use(middleware.static('./public'))
+    app.use(middleware.static('./static'))
     app.use(middleware.notFound())
     app.use(middleware.error())
     app.listen(3000, () => console.log("Expresso application is ready!"))
