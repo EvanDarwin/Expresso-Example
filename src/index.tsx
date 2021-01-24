dfad3c2b3add3b8094b74ffdd3ccf5dbdb154011
Evan Darwin <evan@relta.net>
2021-01-23 20:23:59 -0800

    Expresso version 0.1.1, updated README and index.tsx example

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
-import expresso, {connect, ExpressoRequest, ExpressoResponse, middleware} from "expresso";
+import expresso, {ExpressoRequest, ExpressoResponse, middleware} from "expresso";
 import {h} from "preact";
 import {Homepage} from "./components";
 
-// connect() loads the database connection
-connect().then(() => {
+/**
+ * To begin using TypeORM and loading the database connection, first ensure that
+ * you have configured your `ormconfig.json` properly in the root of your project.
+ *
+ * Next, uncomment the `connect()` and matching line at the end to wrap the application
+ * in the TypeORM connection.
+ */
+
+// connect().then(() => {
+(() => {
     const app = expresso();
-    app.use(middleware.parsers.urlencoded())
+    app.use(middleware.parsers.urlencoded({extended: false}))
     app.use(middleware.helmet())
 
     // demo homepage
@@ -17,4 +25,5 @@ connect().then(() => {
     app.use(middleware.notFound())
     app.use(middleware.error())
     app.listen(3000, () => console.log("Expresso application is ready!"))
-})
+})()
+// })
